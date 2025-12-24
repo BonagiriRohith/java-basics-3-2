20536359c13101874cb6c1941be6dbb57cc29e12
BonagiriRohith <bonagirirohith799@gmail.com>
2025-12-24 10:06:13 +0530

    Array Demo

---
+++ StringMethods.java
@@ -0,0 +1,13 @@
+public class StringMethods {
+    public static void main(String[] args) {
+         String str="CSE";
+         str=str.concat("Delta");
+         System.out.println(str);
+
+        String s =" Computer Science and Engineering DELTA";
+        System.out.println(s.substring(0,8));
+        System.out.println(s.length());
+        System.out.println(s.charAt(9));
+        
+    }
+}
